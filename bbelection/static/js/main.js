e63fbe26d6e39de4b7081b45c79026cf24ab434b
Vikil Naik <vikilnaik37@gmail.com>
2019-03-29 14:53:59 -0400

    Finished part 1 of this excercise

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-});
+    const candidateList = document.querySelector('#candidate_list');
+    axios.get('https://bb-election-api.herokuapp.com/')
+        .then((response) => {
+            response.data.candidates.forEach((e) => {
+                let newList = document.createElement('li');
+                newList.innerText = "Name: " + e.name + "\nVotes: " + e.votes;
+                candidateList.appendChild(newList);
+            })
+
+        })
+})
